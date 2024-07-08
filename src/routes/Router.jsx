d7e779683ad212f59943ab3b7d9a3cbf77c2b160
Abdulaziz Alhaqbani <alhaqbani21@gmail.com>
2024-07-08 21:38:52 +0300

    error page template

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,11 +4,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import SignUpPage from '../pages/SignUpPage';
+import ErrorPage from '../pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: '/login',
