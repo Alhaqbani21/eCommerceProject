7ff822008bd273e33fb335145f765136dfca0121
Abdulaziz Alhaqbani <alhaqbani21@gmail.com>
2024-07-10 01:44:15 +0300

    update add to cart

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { Link, useRouteError } from 'react-router-dom';
 
-function ErrorPage() {
-  return <div>ErrorPage</div>;
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <>
+      <div className="flex items-center justify-center w-screen h-screen">
+        <div className="px-4 lg:py-12">
+          <div className="lg:gap-4 lg:flex">
+            <div className="flex flex-col items-center justify-center md:py-24 lg:py-32">
+              <h1 className="font-bold text-[#E47732] text-9xl">404</h1>
+              <p className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+                <span className="text-red-500">Oops!</span> Page{' '}
+                {error.statusText}
+              </p>
+              <p className="mb-8 text-center text-gray-500 md:text-lg">
+                The page you’re looking for doesn’t exist.
+              </p>
+              <Link
+                to="/"
+                className="btn bg-[#E47732] hover:bg-[#E97739] text-white"
+              >
+                Go home
+              </Link>
+            </div>
+            <div className="mt-4">
+              <img
+                src="https://cdn.pixabay.com/photo/2016/11/22/23/13/black-dog-1851106__340.jpg"
+                alt="img"
+                className="object-cover w-full h-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default ErrorPage;
